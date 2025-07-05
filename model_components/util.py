95f9a2f5c270515c72bee50dbc372dd1dcfe9af6
rtenacity <rtenacity@gmail.com>
2025-07-05 02:50:50 +0000

    reworked Fourier architecture

--- model_components/util.py
+++ model_components/util.py
@@ -14,10 +14,10 @@ def get_data(x_range, y_range, x_num, y_num):
     x_mesh, t_mesh = np.meshgrid(x,t)
     data = np.concatenate((np.expand_dims(x_mesh, -1), np.expand_dims(t_mesh, -1)), axis=-1)
     
-    b_left = data[0,:,:] # x = 0
-    b_right = data[-1,:,:] # x = 1
-    b_upper = data[:,-1,:] # t = 1
-    b_lower = data[:,0,:] # t = 0
+    b_left = data[0,:,:] # t = 0
+    b_right = data[-1,:,:] # t = 1
+    b_upper = data[:,-1,:] # x = 1
+    b_lower = data[:,0,:] # x = 0
     res = data.reshape(-1,2)
 
     return res, b_left, b_right, b_upper, b_lower
